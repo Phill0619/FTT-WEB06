496a48c6db5553d8ae0ace77d22ee76ce34b4451
NIKON-IMPRESSORA <NIKON-IMPRESSORA@10.0.0.112>
2020-06-14 19:56:02 -0300

    moves api

--- src/ftt/ec/api/MovesApi.java
+++ src/ftt/ec/api/MovesApi.java
@@ -1,12 +1,17 @@
 package ftt.ec.api;
 
 import java.io.IOException;
+import java.util.Date;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import ftt.ec.beans.Moves;
+import ftt.ec.model.MovesDAO;
 
 /**
  * Servlet implementation class MovesApi
@@ -42,29 +47,68 @@ public class MovesApi extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		System.out.println("Moves get - " + new Date());
+
+		// TODO Auto-generated method stub
+		Moves m = new Moves();
+		
+		// Note que getParamtere retorna string, 
+		// a conversão é feita na classe Moves(seters)		
+		m.setId(request.getParameter("ID"));
+        m.setName(request.getParameter("NAME"));
+        m.setPower(request.getParameter("NUMBER"));
+        m.setAccuracy(request.getParameter("ACCURACY"));
+        m.setType(request.getParameter("TYPE"));		 
+         
+		MovesDAO movesDAO = new MovesDAO();		
+		movesDAO.insert(m);
+
+        //TODO: Gerenciar e propagar erro...
+		
+		//System.out.println(request.getContentLength());		
+		if (request.getContentLength() < 1) {
+			response.getWriter().append("{\"count\":" + movesDAO.count() + "}");
+		} else {
+			response.getWriter().append("Move created...");
+		} //if
+	} //doGet	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Client post - " + new Date());
 		doGet(request, response);
-	}
+	}//doPost
 
 	/**
 	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
 	 */
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-	}
+		
+		System.out.println("Moves put - " + new Date());
+	}//doPut
 
 	/**
 	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
 	 */
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-	}
-
+		
+		System.out.println("Moves delete - " + new Date());		
+		Moves m = new Moves();				
+		m.setId(request.getParameter("ID"));//getParamtere retorna string, conversão é feita na classe Moves (seters)	
+		System.out.println("Delete: " + m.getId());
+		
+		MovesDAO movesDAO = new MovesDAO();		
+		movesDAO.delete(m);
+		
+		//TODO: Implementar processamento de erro apropriado...
+		response.getWriter().append("Move deleted...");		
+	} //doDelete
 }
